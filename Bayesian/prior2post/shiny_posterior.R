bd68a8a97141ae3e589ceeb7330f8f4ec0ca0046
mc <6876c76f@opayq.com>
2016-10-14 08:02:57 -0400

    update demo

--- Bayesian/prior2post/shiny_posterior.R
+++ Bayesian/prior2post/shiny_posterior.R
@@ -1,15 +1,32 @@
 
-ui <- fluidPage(
-
+ui <- pageWithSidebar(
   # Application title
-  titlePanel("Bayesian Data Analysis"),
+  # titlePanel("Bayesian Data Analysis"),
+  headerPanel(
+    # tags$style("h5 {color:aliceblue;}
+    #               label {color:dodgerblue;"),
+    HTML("<span style=\"color:#ff6eb4; font-family:'Magneto'; \">Bayesian Demo</span>
+
+    <br> <div><h5 style='color:gray'>The following demo regards estimating a mean (ignoring estimating the variance) and how the prior distribution and likelihood combine to produce the posterior distribution. You can set the following parameters: <br><br><br>
+
+    <ul>
+    <li> Sample size: 0-250
+    <li> Observed and Prior Means: 1-10
+    <li> Observed and Prior Variances: 1-5
+    </ul>
+
+
+    The tested &theta; parameters are a sequence of 500 values from 1 to 10.
+    </h5></div>")
+    ),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
+  sidebarPanel(
+      tags$style(".well {background-color:aliceblue;}
+                  label {color:cornflowerblue; font-family:'Gill Sans MT'}"),
       numericInput("n",
                    "Data Sample Size:",
-                   min = 1, max = 250, value = 10, step=10),
+                   min = 0, max = 250, value = 10, step=10),
       numericInput("dataMean",
                    "Data Mean:",
                    min = 1, max = 10, value = 7, step=1),
@@ -17,20 +34,23 @@ ui <- fluidPage(
                    "Data Variance:",
                    min = 1, max = 5, value = 3),
       numericInput("priorMean",
-                   "Prior Mean:",
+                   HTML("Prior Mean for &theta;:"),
                    min = 1, max = 10, value = 2, step=1),
       numericInput("priorVar",
-                   "Prior Variance:",
-                   min = 1, max = 10, value = 1)
+                   HTML("Prior Variance for &theta;:"),
+                   min = 1, max = 5, value = 1)
     ),
 
 
     mainPanel(
-      # plotOutput("bayesPlot", width='50%'), width = 4
-      plotlyOutput("bayesPlot", width = '50%')
-    )
-  )
+      plotly::plotlyOutput("bayesPlot", width = '75%'),
+      htmlOutput('results', span="style=color:red"), br(),
+      htmlOutput("caption"),
+      tags$style("#results {color:cornflowerblue; font-variant:small-caps; font-family:'Consolas';}
+                  #caption {color:gray;}")
+    , width=6)
 )
+
 
 
 
@@ -40,30 +60,53 @@ server <- function(input, output) {
   simN = 500
   theta = seq(1, 10, length.out = simN)
 
-  output$bayesPlot <- renderPlotly({
-    obs  = rnorm(input$n, input$dataMean, sqrt(input$dataVar))
-    prior = data.frame(Distribution='Prior', theta=theta,
-                       density = dnorm(theta, input$priorMean, sqrt(input$priorVar)))
-    like = data.frame(Distribution='Likelihood', theta=theta,
-                      density = sapply(theta, function(parm) exp(sum(dnorm(obs, mean=parm, sd=input$dataVar, log = T)))))
-    denom = sum(like$density*prior$density)
-    post = data.frame(Distribution='Posterior', theta=theta,
-                      density = like$density*prior$density)
+  obs  = reactive({ rnorm(input$n, input$dataMean, sqrt(input$dataVar))})
+  prior = reactive({data.frame(Distribution='Prior', theta=theta,
+                     density = dnorm(theta, input$priorMean, sqrt(input$priorVar)))
+  })
+  like = reactive({data.frame(Distribution='Likelihood', theta=theta,
+                    density = sapply(theta, function(parm) exp(sum(dnorm(obs(), mean=parm, sd=input$dataVar, log = T)))))
+  })
+  denom = reactive({sum(like()$density*prior()$density)})
+  post = reactive({data.frame(Distribution='Posterior', theta=theta,
+                    density = like()$density*prior()$density/denom())
+  })
+  thetamean = reactive({sum(post()$density*theta)})
+  plotdata = reactive({rbind(prior(), like(), post())})
 
-    d = rbind(prior, like, post)
 
-    g = ggplot(aes(x=theta, y=density, group=Distribution, color=Distribution, fill=Distribution), data=d) +
-        geom_ribbon(aes(ymin=0, ymax=density), alpha=.5 ) +
-        facet_wrap(~Distribution, scales = 'free_y', ncol = 1) +
-        lims(x=c(1, 10)) +
-        lazerhawk::theme_trueMinimal() +
-        theme(axis.text.y=element_blank(),
-              axis.title.y=element_blank(),
-              axis.ticks.y=element_blank(),
-              legend.position='none')
 
-    ggplotly(g)
+  output$results = renderText({
+    HTML(paste0("<span style='color:cornflowerblue'>Observed Mean = ", format(mean(obs()), digits=3, nmsall=2), '<br>',
+                "Observed Var = ",  format(var(obs()), digits=3, nmsall=2), '<br>',
+                "Posterior Mean = ",  format(thetamean(), digits=3, nmsall=2))
+         )
+  })
 
+
+  output$bayesPlot = renderPlotly({
+
+    g = ggplot(aes(x=theta, y=density, group=Distribution, color=Distribution, fill=Distribution), data=plotdata()) +
+      geom_ribbon(aes(ymin=0, ymax=density), alpha=.5 ) +
+      geom_point(aes(x=value, y=0), data=data.frame(Distribution=c('Prior', 'Likelihood', 'Posterior'),
+                                                        value=c(input$priorMean, mean(obs()), thetamean())),
+                 color=alpha('#ff5503', .25)) +
+      facet_wrap(~Distribution, scales = 'free_y', ncol = 1) +
+      xlab(HTML('&theta;')) +
+      lims(x=c(1, 10)) +
+      lazerhawk::theme_trueMinimal() +
+      theme(axis.title.x=element_text(color=alpha('black',.6), vjust=.1, hjust=.5),
+            axis.text.y=element_blank(),
+            axis.title.y=element_blank(),
+            axis.ticks.y=element_blank(),
+            strip.text=element_text(color=alpha('black',.5), vjust=.01),
+            legend.position='none')
+
+    ggplotly(g, tooltip='none')
+
+  })
+  output$caption <- renderText({
+    HTML('All three distributions regard the <i>parameter</i> to be estimated, i.e. &theta;, the mean.  The prior regards the initial distribution given for &theta;. This may be based on prior beliefs and/or research, or simply one known to work well within the modeling context.  The likelihood regards the data given a particular estimate for &theta;, and is the same that one is familiar with from standard maximum likelihood methods. Finally, the posterior is the final likelihood for the &theta; values.')
   })
 }
 
